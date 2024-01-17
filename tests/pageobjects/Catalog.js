f1a4c9a3db34d325de94cf477fbe0019a9c5abf4
kvale17 <kevin.valencia24@gmail.com>
2024-01-17 15:45:43 -0800

    Add catalog class

---
+++ tests/pageobjects/Catalog.js
@@ -0,0 +1,34 @@
+const { expect } = require("@playwright/test");
+class Catalog {
+  constructor(page) {
+    this.page = page;
+  }
+
+  async addProductToCart(name, size, color) {
+    const product = this.page.locator(`.product-item-info:has-text('${name}')`);
+
+    await product.getByRole("option", { name: size }).click();
+    await product.getByRole("option", { name: color }).click();
+    await product.getByRole("button", { name: "Add to Cart" }).click();
+  }
+
+  async assertMiniCartCount(count) {
+    await expect(this.page.locator(".counter-number")).toHaveText(
+      count.toString()
+    );
+  }
+  async goToCatalog(gender, category, type) {
+    await this.page.getByRole("menuitem", { name: " " + gender }).hover();
+    await this.page.getByRole("menuitem", { name: category }).hover();
+    await this.page.getByRole("menuitem", { name: type }).click();
+  }
+
+  async assertProductInCart(name) {
+    await this.page.locator(".showcart").click();
+
+    const miniCartDialog = this.page.locator(".block-minicart");
+
+    await expect(miniCartDialog.locator(".product-item-name")).toHaveText(name);
+  }
+}
+module.exports = { Catalog };
