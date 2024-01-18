1b85ce5b087e99b20029050e63777ffd93243e4b
kvale17 <kevin.valencia24@gmail.com>
2024-01-17 18:04:05 -0800

    Take screenshot if test fails

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -1,4 +1,4 @@
-const { Before } = require("@cucumber/cucumber");
+const { Before, After, Status } = require("@cucumber/cucumber");
 const playwright = require("@playwright/test");
 
 Before(async function () {
@@ -7,4 +7,17 @@ Before(async function () {
   });
   const context = await browser.newContext();
   this.page = await context.newPage();
+});
+
+After(async function ({ result }) {
+  if (result.status === Status.FAILED) {
+    const buffer = await this.page.screenshot();
+
+    await this.page.screenshot({
+      path: "test-results/screenshots/screenshot.png",
+    });
+
+    this.attach(buffer.toString("base64"), "base64:image/png");
+    console.log("Screenshot logged");
+  }
 });
