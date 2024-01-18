9857290bf405e3b10caac2839620048564fccb59
kvale17 <kevin.valencia24@gmail.com>
2024-01-17 17:18:22 -0800

    Add steps for checkout feature

---
+++ features/step_definitions/checkoutSteps.js
@@ -0,0 +1,25 @@
+const { Given, When, Then } = require("@cucumber/cucumber");
+const { Catalog } = require("../../tests/pageobjects/Catalog");
+
+const productName = "Proteus Fitness Jackshirt";
+
+Given(
+  "the user is viewing the catalog", { timeout: 30 * 1000 }, async function () {
+    await this.page.goto("https://magento.softwaretestingboard.com/");
+
+    this.catalog = new Catalog(this.page); 
+
+    await this.catalog.goToCatalog("Men", "Tops", "Jackets");
+  }
+);
+
+When("the user selects a products options and clicks Add to Cart", async function () {
+    this.catalog.addProductToCart(productName, "XS", "Black");
+  }
+);
+
+Then("then the specified product is added to the cart", async function () {
+  await this.catalog.assertMiniCartCount(1);
+
+  await this.catalog.assertProductInCart(productName);
+});
