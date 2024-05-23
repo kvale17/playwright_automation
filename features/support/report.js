2a949c2f5d51f946fb8ef75bdb88b3ed15188a8c
kvale17 <kevin.valencia24@gmail.com>
2024-05-23 13:10:56 -0700

    Fix typo

--- features/support/report.js
+++ features/support/report.js
@@ -3,7 +3,7 @@ var reporter = require("cucumber-html-reporter");
 var options = {
   theme: "bootstrap",
   jsonFile: "test-results/cucumber_report.json",
-  output: "playwright-report/cucumber-report/cucmber-report.html",
+  output: "playwright-report/cucumber-report/cucumber-report.html",
   reportSuiteAsScenarios: true,
   scenarioTimestamp: true,
   launchReport: false,
